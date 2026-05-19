24c969b750548bcfd89984c1396ef03463b6876e
supri <supriyasawant3612@gmail.com>
2026-05-19 09:03:23 +0530

    Add JavaScript functions and variable concepts chapter

---
+++ Chapter_04_Javascripts_concepts/10_Functions.js
@@ -0,0 +1,12 @@
+//Define of the function
+function greet() {
+    console.log("Hello, how are you?");
+}
+
+
+//Calling the function
+greet();
+greet();
+greet();
+greet();
+greet();
---
+++ Chapter_04_Javascripts_concepts/11_var_concepts.js
@@ -0,0 +1,5 @@
+var v = 10;
+
+var browser = "chrome";
+var browser = "firefox"; // re-declaring the variable is allowed
+browser = "edge"; // re-assigning the variable is allowed
