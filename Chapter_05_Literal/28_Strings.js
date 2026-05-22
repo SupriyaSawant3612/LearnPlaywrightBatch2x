dc68ddbfcbc1f2986e6fd3735125c76754888294
supri <supriyasawant3612@gmail.com>
2026-05-22 13:20:18 +0530

    Add strings literal example

---
+++ Chapter_05_Literal/28_Strings.js
@@ -0,0 +1,9 @@
+let single = 'Hello World';
+let double = "Hello World";
+
+let c = 'c';
+let c1 = 'cc';
+console.log("typeof c):", typeof c); // "string"
+console.log("typeof c1):", typeof c1); // "string"  
+
+//Javascripts prefer to use single code 
