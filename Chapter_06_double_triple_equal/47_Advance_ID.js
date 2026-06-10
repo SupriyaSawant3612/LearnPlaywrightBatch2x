0bdc32e34ab5a4f93726dfae5db4664d7eb690da
supri <supriyasawant3612@gmail.com>
2026-06-10 11:41:02 +0530

    Add advance increment/decrement expression example

--- Chapter_06_double_triple_equal/47_Advance_ID.js
+++ Chapter_06_double_triple_equal/47_Advance_ID.js
@@ -11,3 +11,7 @@ console.log(a); //12 (a is now 12 after the post-increment and pre-increment)
 let a = 10;
 console.log(++a + ++a); //22 (pre-increment: increments the value first, then returns it)
 console.log(a); //12 (a is now 12 after the pre-increment and post-increment)
+
+let a = 5;
+console.log(a++ + ++a - --a + a-- + ++a); //11 (post-increment: returns the value first, then increments it)
+console.log(a); //7 (a is now 7 after all the increments and decrements)
